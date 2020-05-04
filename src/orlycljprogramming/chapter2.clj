cc722e02a143838347b6043bb9d109cb00d43f1b
silkyprogrammer <hpalappe@gmail.com>
2020-05-04 18:00:25 +0530

    functions as arguments (first class treatment of functions).

--- src/orlycljprogramming/chapter2.clj
+++ src/orlycljprogramming/chapter2.clj
@@ -10,3 +10,12 @@
 (h [1 2])
 (conj (first (keys h)) 3)
 (h [1 2])
+
+; calling a function twice
+(defn call-twice
+  "do anything I ask twice"
+  [f x]
+  (f x)
+  (f x)
+  )
+(call-twice println 123)
